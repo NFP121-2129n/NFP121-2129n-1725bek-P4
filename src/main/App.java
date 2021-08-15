8f935ac0311fb215247c2d909557a6d26cf9d3c2
Georges Ghafary <georges.ghafary@isae.edu.lb>
2021-08-15 09:21:12 +0300

    commit

--- src/main/App.java
+++ src/main/App.java
@@ -3,6 +3,8 @@ package main;
 import java.awt.*;
 import java.util.List;
 import java.awt.event.*;
+import java.io.*;
+
 import javax.swing.*;
 
 import models.Classe;
@@ -109,6 +111,32 @@ public class App implements ActionListener {
             panel = new HoraireView().mainPanel;
             frame.setContentPane(panel);
             frame.pack();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    void loadData() {
+        File directory = new File("../../data");
+        if (directory.isDirectory() && directory.exists()) {
+            FileInputStream fis;
+            ObjectInputStream ois;
+            try {
+                File fileCla = new File(directory.getName() + "/Classes");
+                if (fileCla.exists()) {
+                    fis = new FileInputStream(fileCla);
+                    ois = new ObjectInputStream(fis);
+                    listCla = (ArrayList<Classe>) ois.readObject();
+                    ois.close();
+                    if (!listCla.isEmpty()) {
+
+                    }
+                }
+            } catch (IOException | ClassNotFoundException ex) {
+                System.out.println(ex);
+            }
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Répertoire non-trouvée!.");
         }
     }
 
--- src/models/Horaire.java
+++ src/models/Horaire.java
@@ -1,6 +1,39 @@
 package models;
 
-import java.util.*;
-
 public class Horaire {
+    private static int counter = 1;
+    private int id;
+    private String campus;
+    private Classe horaire[][];
+
+    public Horaire(String campus, Classe horaire[][]) {
+        this.setId(counter++);
+        this.setCampus(campus);
+        this.setHoraire(horaire);
+    }
+
+    public Classe[][] getHoraire() {
+        return horaire;
+    }
+
+    public void setHoraire(Classe horaire[][]) {
+        this.horaire = horaire;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
